5a81e111213737eb5519903b43dd059c9ba3b0ec
iSpeX1 <estebangeley@gmail.com>
2022-11-17 12:15:15 +0100

    fin séance 17/11

---
+++ src/main/java/Model/Message.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Message {
+    private static String data;
+    private static String time;
+
+    public static void message(){
+
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+
+
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,8 @@
 public class main {
+
+    public static void main (String[] args){
+        System.out.println("hello world");
+    }
+
 }
+
