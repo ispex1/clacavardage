63b03c4dbdae96062d28b29b68e884e0cc85ccf3
iSpeX1 <ispexgaming@gmail.com>
2022-11-29 16:58:49 +0100

    debut option recherche bdd

--- src/main/java/Database/DatabaseManager.java
+++ src/main/java/Database/DatabaseManager.java
@@ -1,8 +1,11 @@
+
 package database;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,8 +22,7 @@ public class DatabaseManager {
     public DatabaseManager(){
         createNewDatabase();
         connect();
-        //createTableConvo("user");
-        createNewTable("user");
+        createNewConvo("gaboche");
     }
 
     // utile ??? 
@@ -35,7 +37,6 @@ public class DatabaseManager {
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -47,7 +48,6 @@ public class DatabaseManager {
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             System.out.println("Connection to SQLite has been established.");
-            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -61,7 +61,7 @@ public class DatabaseManager {
         }
     }
 
-    public static void createNewTable(String ipOther) {
+    public static void createNewConvo(String ipOther) {
         // SQL statement for creating a new table
         String sql= "CREATE TABLE IF NOT EXISTS " + ipOther +"(\n"
                 + "	indexMsg integer PRIMARY KEY,\n" // index du message
@@ -70,17 +70,65 @@ public class DatabaseManager {
                 + "	time text NOT NULL"// date et heure
                 + ");"; 
 
-        // SQL statement for creating a new table
         try (Connection conn = DriverManager.getConnection(url); 
              Statement  stmt = conn.createStatement()) {
                 // create a new table
                 stmt.execute(sql);
-                System.out.println("A new table has been created with the name : " + ipOther);
+                System.out.println("A new convo has been created with the name : " + ipOther);
         } catch (SQLException e) {
             System.out.println("Error creating table");
             System.out.println(e.getMessage());
         }
     }
+
+    public static void deleteConvo(String ipOther) {
+        // SQL statement for deleting a table
+        String sql= "DROP TABLE IF EXISTS " + ipOther; 
+
+        try (Connection conn = DriverManager.getConnection(url); 
+             Statement  stmt = conn.createStatement()) {
+                // delete the table
+                stmt.executeUpdate(sql);
+                System.out.println("The " + ipOther + " table has been successfully deleted");
+        } catch (SQLException e) {
+            System.out.println("Error deleting table");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertMessage(String ipOther, Message msg) {
+        String sql = "INSERT INTO " + ipOther + "(indexMsg,sender,message,time) VALUES(?,?,?,?)";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(2, msg.getSender().getIP());
+                pstmt.setString(3, msg.getData());
+                pstmt.setString(4, msg.getTime());
+                pstmt.executeUpdate();
+                System.out.println("A new message has been add to the " + ipOther + " table");
+        } catch (SQLException e) {
+            System.out.println("Error insert a message");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // the function return the indexMsg of rows that have the data string into it
+    // for the moment, it return the first indexMsg that match
+    public int findMessage(String ipOther, String data){
+        String sql = "SELECT indexMsg FROM " + ipOther + " WHERE message = ?";
+        int index = -1;
+
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, data);
+                pstmt.executeQuery();
+                System.out.println("The index of the message is : " + index);
+        } catch (SQLException e) { 
+            System.out.println("Error finding a message");
+            System.out.println(e.getMessage());
+        }
+        return index;
+    }    
 
     public static void archiveMessage(int idSession, Message msg){
         //BDD(idSession).add(msg); //requête JDBC
