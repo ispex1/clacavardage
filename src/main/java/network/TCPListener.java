c2e324339cda50df655cb0df3fe22033f0a96004
Bordais-Poulard Gabriel <bordais-@insa-10219>
2022-12-09 17:44:07 +0100

    TCP listener and Session update, not finished, not build

--- src/main/java/network/TCPListener.java
+++ src/main/java/network/TCPListener.java
@@ -1,5 +1,54 @@
 package network;
 
-public class TCPListener {
-    
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPListener extends Thread {
+
+    // ** ATTRIBUTES **
+    private boolean isRunning;
+    private int port;
+
+    /**
+     * Constructor of the TCPListener class
+     * TCP Listener is just a thread that listen for incoming TCP connections
+     * @param port
+     */
+    public TCPListener(int port){
+        setPort(port);
+        start();
+    }
+
+    /**
+     * run method
+     */
+    public void run(){
+        this.isRunning = true;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(isRunning){
+                Socket link = serverSocket.accept();
+                TCPSession session = new TCPSession(link);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // ** GETTERS AND SETTERS**
+    public boolean isRunning() {
+        return isRunning;
+    }
+    public void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+    public int getPort() {
+        return port;
+    }
+    public void setPort(int port) {
+        this.port = port;
+    }
 }
--- src/main/java/network/TCPSession.java
+++ src/main/java/network/TCPSession.java
@@ -1,5 +1,61 @@
 package network;
 
-public class TCPSession {
-    
+import model.User;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TCPSession extends Thread{
+
+    // ** ATTRIBUTES **
+    private Socket socket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    /**
+     * Constructor of the TCPSession when a use start a session with us
+     * @param link
+     */
+    public TCPSession(Socket link) {
+        setSocket(link);
+        start();
+    }
+
+    /**
+     * Constructor of the TCPSession when we start a session with a user
+     */
+    public TCPSession(User userdist) {
+        try {
+            setSocket(new Socket(userdist.getIP(), userdist.getPort()));
+            setInputStream(new ObjectInputStream(socket.getInputStream()));
+            setOutputStream(new ObjectOutputStream(socket.getOutputStream()));
+            start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void run(){
+        //TODO
+    }
+
+    // ** GETTERS AND SETTERS **
+    public Socket getSocket() {
+        return socket;
+    }
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+    public void setInputStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+    public OutputStream getOutputStream() {
+        return outputStream;
+    }
+    public void setOutputStream(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
 }
