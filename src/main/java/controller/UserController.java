ae18683eecd21ca6e4e386a2b96690f8faae893b
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2023-01-18 22:56:45 +0100

    fix bug change mdp not notified

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -140,7 +140,7 @@ public class UserController {
         String[] splitedMsg = msg.split("\\|");
 
         // Filtrage des messages recu et ejection des messages non conformes
-        String[] typeMsgString = {"ASK_PSEUDO","PSEUDO_OK","PSEUDO_NOT_OK","CONNECT","DISCONNECT","ASK_USER_LIST","USER_LIST","TEST"};
+        String[] typeMsgString = {"ASK_PSEUDO","PSEUDO_OK","PSEUDO_NOT_OK","PSEUDO","CONNECT","DISCONNECT","ASK_USER_LIST","USER_LIST","TEST"};
         if (!Arrays.asList(typeMsgString).contains(splitedMsg[0])){
             System.out.println("Message non conforme" );
             System.out.println("Message recu : " + msg);
